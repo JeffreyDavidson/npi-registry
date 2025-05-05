c97fea89cf0ab52c2bd3a80eda0a51d0d949468f
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-05-04 22:49:22 -0400

    Refactor component

--- app/Livewire/ProviderList.php
+++ app/Livewire/ProviderList.php
@@ -2,33 +2,50 @@
 
 namespace App\Livewire;
 
+use App\Models\ProviderInstance;
 use Illuminate\Support\Facades\Http;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class ProviderList extends Component
 {
-    public array $providers = [];
-
     /**
      * The Livewire Form that contains the public properties and validation for the search
      */
     public ProviderForm $providerForm;
 
-    public function mount()
+    /**
+     * Function to retrieve providers
+     *
+     * @param  array<mixed>  $validated
+     * @return array<mixed>
+     */
+    protected function getProviders(array $validated): array
     {
-        $this->providers = $this->getProviders();
+        $response = Http::get('https://npiregistry.cms.hhs.gov/api', array_merge(['version' => '2.1', 'limit' => '50'], $validated));
+
+        $resultList = [];
+        foreach ($response->json()['results'] as $result) {
+            $resultList[] = new ProviderInstance($result['number'], $result['basic'], $result['taxonomies'], $result['addresses']);
+        }
+
+        return $resultList;
     }
 
-    public function getProviders(): array
+    /**
+     * Function to retrieve providers
+     */
+    public function findProviders(): void
     {
-        return Http::get('https://npiregistry.cms.hhs.gov/api', [
-            'version' => '2.1',
-            'first_name' => '',
-            'last_name' => '',
-            'city' => 'baltimore',
-            'state' => '',
-            'postal_code' => '',
-        ])->json()['results'];
+        $this->providerForm->validate();
+    }
+
+    #[Computed]
+    public function providers(): array
+    {
+        $validated = $this->providerForm->validate();
+
+        return array_filter($validated) ? $this->getProviders($validated) : [];
     }
 
     public function render()
